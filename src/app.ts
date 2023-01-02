b3863126abda0d99b60a7ed9add667b82916cd8d
j12y <jayson.delancey@gmail.com>
2023-01-02 10:39:03 -0800

    Use liquid to render a README with content substitution

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,28 @@
 
-"use strict";
+import dotenv from 'dotenv';
+import { Liquid } from 'liquidjs'
 
-let message: string = 'Hello, World!';
+dotenv.config();
 
-console.log(message);
+const engine = new Liquid({
+  root: __dirname,
+  extname: '.liquid'
+})
+
+// TODO: lookup dynamic data
+
+const scope = {
+  message: 'Hello World'
+}
+
+
+// TODO: write file to disk
+engine.renderFile('README', scope).then(console.log)
+
+
+
+/*
+
+import path from 'path';
+
+*/
