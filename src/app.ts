a0701323bad33434aabecd63c8c5893c59c7ef56
j12y <jayson.delancey@gmail.com>
2023-02-12 12:29:13 -0800

    github REST api and GraphQL to fetch data

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,60 @@
 
 import dotenv from 'dotenv';
+
 import { Liquid } from 'liquidjs'
 import { writeFileSync } from 'fs';
 import path from 'path';
 
-dotenv.config();
+import { GitHub } from "./gh";
 
-// Populate dynamic data
-// TODO: lookup dynamic data
-const scope = {
-  generated: new Date().toDateString(),
+
+export async function main() {
+  dotenv.config();
+
+  // Populate dynamic data
+  const user: string = process.env.USERNAME || '';
+  const repo: string = process.env.REPO || '';
+
+  let gh = new GitHub();
+  // let user_details = await gh.getUserDetails(user);
+
+  const scope = {
+    generated: new Date().toDateString(),
+  //  followers: user_details.followers,
+  //  public_repos: user_details.public_repos
+  }
+
+
+  let result = await gh.getReposDates();
+  console.log(result);
+  return;
+
+  // let repos_info = await gh.getReposInfo(user, repo);
+  // console.log(repos_info.created_at);
+  // console.log(repos_info.updated_at);
+
+  // getReposMetrics();
+  // getReposReferrers();
+  // getReposTags();
+  // getReposTopics();
+  // getUserFollowers();
+
+
+  // Identify template location for engine
+  // Learn more: https://liquidjs.com/
+  const engine = new Liquid({
+    root: path.resolve(__dirname, 'template/'),
+    extname: '.liquid'
+  })
+
+  // Write the newly generated README file to disk
+  engine.renderFile('README', scope).then((content) => {
+      writeFileSync(path.join(__dirname, '../README.md'), content, {
+          flag: 'w'});
+  });
+
+
 }
 
-// Identify template location for engine
-// Learn more: https://liquidjs.com/
-const engine = new Liquid({
-  root: path.resolve(__dirname, 'template/'),
-  extname: '.liquid'
-})
 
-// Write the newly generated README file to disk
-engine.renderFile('README', scope).then((content) => {
-    writeFileSync(path.join(__dirname, '../README.md'), content, {
-        flag: 'w'});
-});
-
+main();
--- src/gh.ts
+++ src/gh.ts
@@ -1,75 +1,99 @@
 
 import { Octokit } from 'octokit';
 import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods';
+const { graphql } = require("@octokit/graphql");
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-// getReposInfo();
-// getReposMetrics();
-// getReposReferrers();
-// getReposTags();
-// getReposTopics();
-// getUserFollowers();
-getUserDetails();
+export class GitHub {
+    graphqlWithAuth = graphql.defaults({
+        headers: {
+            authorization: `token ${process.env.GITHUB_TOKEN}`
+        }
+    });
+
+    // GET /graphql
+    // https://docs.github.com/en/graphql
+    async getQueryResult(query: string): Promise<string> {
+        const { repository } = await this.graphqlWithAuth(query);
+        return repository;
+    }
+
+    async getReposDates(): Promise<string> {
+        return this.getQueryResult(`
+            { 
+                repository(name: "j12y", owner: "j12y") { 
+                    createdAt, updatedAt 
+                }
+            }
+        `);
+    }
+
+
+
+    // GET /users/{user}
+    // https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#get-a-user
+    async getUserDetails(user: string): Promise<RestEndpointMethodTypes['users']['getByUsername']['response']['data']> {
+        const { data } = await octokit.rest.users.getByUsername({
+            username: user
+        });
+
+        return data;
+    };
+
+    // GET /repos/{owner}/{repos}
+    // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#get-a-repository
+    async getReposInfo(user: string, repo: string): Promise<RestEndpointMethodTypes['repos']['get']['response']['data']> { 
+        const { data } = await octokit.rest.repos.get({
+            owner: user,
+            repo: repo
+        });
+
+        return data;
+    };
+
+
+}
+
+
 
 /*
-export async function getReposInfo(): Promise<RestEndpointMethodTypes['repos']['get']['response']['data']> { 
-    const { data } = await octokit.rest.repos.get({
-        owner: process.env.USERNAME,
-        repo: process.env.REPO
-    });
 
-    console.log(data);
-    return data;
-}
-
-export async function getReposMetrics(): Promise<RestEndpointMethodTypes['repos']['getViews']['response']['data']> {
+export async function getReposMetrics(user: string, repo: string): Promise<RestEndpointMethodTypes['repos']['getViews']['response']['data']> {
     const { data } = await octokit.rest.repos.getViews({
-        owner: process.env.USERNAME,
-        repo: process.env.REPO
+        owner: user,
+        repo: repo
     });
 
-    console.log(data);
     return data;
 }
 
-export async function getReposTags(): Promise<RestEndpointMethodTypes['repos']['listTags']['response']['data']> {
+export async function getReposTags(user: string, repo: string): Promise<RestEndpointMethodTypes['repos']['listTags']['response']['data']> {
     const { data } = await octokit.rest.repos.listTags({
-        owner: process.env.USERNAME,
-        repo: process.env.REPO
+        owner: user,
+        repo: repo
     });
 
-    console.log(data);
     return data;
 }
 
-export async function getReposTopics(): Promise<RestEndpointMethodTypes['repos']['getAllTopics']['response']['data']> {
+export async function getReposTopics(user: string, repo: string): Promise<RestEndpointMethodTypes['repos']['getAllTopics']['response']['data']> {
     const { data } = await octokit.rest.repos.getAllTopics({
-        owner: process.env.USERNAME,
-        repo: process.env.REPO
+        owner: user,
+        repo: repo
     });
 
-    console.log(data);
     return data;
 }
 
-export async function getUserFollowers(): Promise<RestEndpointMethodTypes['users']['listFollowersForUser']['response']['data']> {
+export async function getUserFollowers(user: string): Promise<RestEndpointMethodTypes['users']['listFollowersForUser']['response']['data']> {
     const { data } = await octokit.rest.users.listFollowersForUser({
-        username: process.env.USERNAME,
+        username: user
     });
 
-    console.log(data);
     return data;
 }
 
 */
 
-export async function getUserDetails(): Promise<RestEndpointMethodTypes['users']['getByUsername']['response']['data']> {
-    const { data } = await octokit.rest.users.getByUsername({
-        username: process.env.USERNAME
-    });
-
-    console.log(data);
-    return data;
-}
 
